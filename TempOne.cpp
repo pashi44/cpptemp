320a6c1191b9a1b5afa4f0e85bd9a8a7bddbeb6d
pashi44 <pashireddi@gmail.com>
2024-12-11 17:21:13 -0600

    added function pointer  with  defeination  and called

--- TempOne.cpp
+++ TempOne.cpp
@@ -36,6 +36,8 @@ int main()
     std::unique_ptr<Without<int, void>> m;
     m = make_unique<Without<int, void>>(1296,callablefunction);
     int recie = m->getAttributes();
+m->callfunction(nullptr);
+
 }
 
 
