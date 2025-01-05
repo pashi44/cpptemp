c8f28890251652b212b3daebe8dbedb09a0a4e51
pashi44 <pashireddi@gmail.com>
2025-01-05 01:01:30 -0600

    too much poiter  recusrion with STL containers

--- thre/include/threadplay.hpp
+++ thre/include/threadplay.hpp
@@ -6,26 +6,20 @@
 #include <vector>
 #include <thread>
 #include <iostream>
-
 #include "ThreadOne.hpp"
-
 using namespace std;
 using namespace threadone;
-
 class AbstarctClass
 {
 public:
     virtual void getnothing() = 0;
     virtual ~AbstarctClass() = default;
 };
-
 namespace threadplay
 {
-
     template <typename T, typename S>
     class Threadplay : public AbstarctClass
     {
-
     public:
         T obj1; //  passing refe ffrom the caller
         S obj2;
@@ -36,15 +30,12 @@ namespace threadplay
         {
             std::cout << "Addresses: obj1: " << (void *)&ob1 << ", obj2: " << (void *)&ob2 << '\n';
         }
-
         std::string &
         returnS()
         {
-            obj2 = std::string("ashdjfasjhdvahsjd");
-
+            obj2 = nullptr;
             return (*this).obj2;
         }
-
         ~Threadplay()
         {
             cout << "dcalss  called\n"
@@ -52,11 +43,9 @@ namespace threadplay
         }
         void getnothing() override
         {
-
             cout << "from dclass\n"
                  << endl;
         }
     };
 }
-
 #endif
