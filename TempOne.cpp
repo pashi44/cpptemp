3fd80716ef91f33ac38b1d35ff20974a9a2c11a4
pashi44 <pashireddi@gmail.com>
2025-01-07 17:45:57 -0600

    added with classs conditional_varaible parameteres

--- TempOne.cpp
+++ TempOne.cpp
@@ -9,33 +9,37 @@
 
 #include <chrono>
 #include <unistd.h>
-#include <sys/types.h>
-
-#include <sys/wait.h>
-#include <vector>
-#include <thread>
-#include <stdexcept>
-#include <iostream>
-#include <cassert>
-#include <queue>
-
-#include <condition_variable>
 // #include "IterOne.hpp"
 #include "threadThreats.hpp"
-
 using namespace std;
 pid_t pid;
-
 int makme = 1296;
-std::queue<threadThreats> shared_dataqueue;
-std::mutex mutexOne;
-std::recursive_mutex rm;
-std::condition_variable data_cond;
+// std::queue<threadThreats> shared_dataqueue;
 
 auto mainThreadID = std::this_thread::get_id();
 
 int main()
 {
+
+    threadThreats threadManager;
+
+    // Producer thread
+    std::thread producer([&]()
+                         {
+        for (int i = 1; i <= 5; ++i) {
+            threadManager.produce_data(i, "data_" + std::to_string(i));
+            std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Simulate work
+        } });
+
+    // Consumer thread
+    std::thread consumer([&]()
+                         {
+        for (int i = 1; i <= 5; ++i) {
+            threadManager.consume_data(process_function);
+        } });
+
+    producer.join();
+    consumer.join();
 
     return 0;
 }
--- iterator/include/threadThreats.hpp
+++ iterator/include/threadThreats.hpp
@@ -4,31 +4,64 @@
 #include <iostream>
 #include <mutex>
 #include <memory>
+#include <condition_variable>
+#include <chrono>
+#include <sys/types.h>
+
 using namespace std;
+
 class DataWrapper
 {
 private:
-    int a;
-    string b;
+    int8_t a = 0;
+    string b = "Datawrapper Default string";
 
 public:
-    void do_something();
+    void do_something()
+    {
+        cout << "from DataWrapper: a = " << a << ", b = " << b << endl;
+    }
+    void set_data(int A, const string &B)
+    {
+        a = A;
+        b = B;
+    }
 };
+
 class threadThreats
 {
 private:
     DataWrapper data;
-    std::mutex mutexTwo;
+    bool dataready = false;
+    std::mutex classmutex;
+    std::condition_variable cv;
 
 public:
-    template <typename Function>
-    void Process_data(Function funObj)
+    // Notify when data is ready to be processed
+    void
+    produce_data(int newA, const string &newB)
     {
+        {
+            std::lock_guard<std::mutex> lock(classmutex);
+            data.set_data(newA, newB);
+            dataready = true;
+        }
+        cv.notify_one(); // Notify a waiting thread
+    }
+    // Wait until data is ready and then process it
+    template <typename Function>
+    void consume_data(Function funObj)
+    {
+        std::unique_lock<std::mutex> lock(classmutex);
+        cv.wait(lock, [this]()
+                { return dataready; }); // Wait until dataReady is true
+        funObj(data);
+        dataready = false; // Reset flag for next production
     }
 };
-DataWrapper *unpotected;
-void malicious_function(DataWrapper &protected_data)
+void process_function(DataWrapper &protected_data)
 {
-    unpotected = &protected_data;
+    protected_data.do_something();
 }
+
 #endif // !  __THREADSTHREATSHPP__
