c10b062ba8731e00581bace7e44e1c9a5d3e8937
pashi44 <pashireddi@gmail.com>
2025-10-16 22:01:37 -0500

    coverved set  and map

--- dsa/include/VectorOne.hpp
+++ dsa/include/VectorOne.hpp
@@ -5,6 +5,9 @@
 #include <memory>
 #include <vector>
 
+
+
+
 using namespace std;
 
 template <typename T, typename Alloc = std::allocator<T>, size_t size = 10>
@@ -12,17 +15,30 @@ class VectorOne
 {
 private:
     T obja;     
-    T* oj;      
+    T* oj;     
+ 
 
 public:
     VectorOne() : obja{}, oj(nullptr) {}
+VectorOne(const  T &&val) : obja(std::move(val)), oj(nullptr) { 
 
+
+
+
+
+
+
+
+} 
     explicit VectorOne(const T& val) : obja(val), oj(nullptr) {}
 
     VectorOne(const VectorOne& other) : obja(other.obja), oj(nullptr) {}
 
     VectorOne(VectorOne&& other) noexcept : obja(std::move(other.obja)), oj(nullptr) {}
 
+
+
+    
     VectorOne& operator=(const VectorOne& other)
     {
         if (this != &other)
--- src/TempOne.cpp
+++ src/TempOne.cpp
@@ -1,38 +1,62 @@
-        #include <iostream>
-        // #include "libOne.hpp"
-        #include <memory>
-        #include <list>
-        #include <string.h>
-        #include <stdlib.h>
-        #include <sys/types.h>
-        #include <unistd.h>
-        #include<algorithm>
-        #include <sys/wait.h>
-    #include <mutex>
-        // #include "libOne.hpp"
-        #include <thread>
-        #include <chrono>
-        #include  "VectorOne.hpp"
-        #include <vector>
-        #include <array>
+            #include <iostream>
+            #include "VectorOne.hpp"
+            #include <memory>
+            #include <string.h>
+            #include <stdlib.h>
+            #include <sys/types.h>
+            #include <unistd.h>
+            #include<algorithm>
+            #include <sys/wait.h>
+        #include <mutex>
+        #include <print>
+#include <map>
+#include <set>
 
-        using namespace std;
-        int main(){
-
-            std::srand(std::time(0)); 
-            pid_t pid;
-            int makme = 1296;
-            int makma =   2048;
-
-            std::mutex mutexOne;    
-            std::string riley = "simon riely";
-    
-    
-
-
-
-
-
-
-        return 0;
+struct CompareVectorOne {
+    bool operator()(const VectorOne<std::string> &a, const VectorOne<std::string> &b) const {
+        return a.getObjectValue() < b.getObjectValue();
     }
+};
+
+
+
+            using namespace std;
+            int main(){
+
+                    std::srand(std::time(0)); 
+                pid_t pid;
+                int makme = 1296;
+                int makma =   2048;
+
+                std::string riley = "simon riely";
+                std::mutex mutexOne;    
+        
+                std::set<VectorOne<std::string> , CompareVectorOne>  set{
+                    
+                    
+  VectorOne<std::string>(std::string("simon riely")),
+  VectorOne<std::string>(std::string("prashanth Nuthula")),
+  VectorOne<std::string>(std::string("Mounika Nuthula ")),
+  VectorOne<std::string>(std::string("varsha reddy polla ")),
+  VectorOne<std::string>(std::string("krishna reddy")),
+
+
+                };
+set.insert(VectorOne<std::string>(std::string("Akhil Nuthula")));
+
+                 
+                for(auto & k :  set){
+cout << k.getObjectValue() << endl;
+
+
+                }
+                
+
+
+
+
+
+
+
+            return 0;
+        }
