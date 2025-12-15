81993c7c9ff6c461c098c433c8fc2d663b48a626
pashi44 <pashireddi@gmail.com>
2025-12-15 14:49:54 -0500

    added basic pointer

--- TempOne.cpp
+++ TempOne.cpp
@@ -1,6 +1,7 @@
 #include <string.h>
 #include "threadplay.hpp"
 #include <stdlib.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <vector>
@@ -10,55 +11,28 @@
 #include <cassert>
 #include "IterOne.hpp"
 using namespace std;
+using namespace Iterone;
 using namespace threadone;
 using namespace threadplay;
 int main()
 {
-    int makme = 1296;
-    std::mutex mutexOne;
-    std::vector<Iterone::IterOne<int, std::string>> IterplayVector;
-    std::string name = "simon riely";
 
-    using IterPair = Iterone::IterOne<int, std::string>; // Alias the Iter  class
-    using Iter = std::vector<IterPair>::iterator;        //  alias the  iter  class into  vector class  and the iteratoe
+IterOne<string, nullptr_t> iterobj{std::string("TestString"), nullptr};
 
-    std::thread th1([&]()
-                    {
-     try
-     {
-         std::lock_guard<std::mutex> lock(mutexOne);
-     
-     IterplayVector.emplace_back(37, "SOAP Mac tavish");
-     
-     }
-     
-         catch (const std::exception &e)
-                 {
-             std::cerr << "Exception caught in thread: " << e.what() << std::endl;
-         } });
-    std::thread th2([&]()
-                    {
-     try
-     {
-         std::lock_guard<std::mutex> lock(mutexOne);
-     
-     IterplayVector.emplace_back(42, "cpatain price");
-     
-     }  
-     
-         catch (const std::exception &e1)
-                 {
-             std::cerr << "Exception caught in thread: " << e1.what() << std::endl;
-         } });
+auto   * iterpointer  =  &iterobj;
 
-    th1.join();
-    th2.join();
 
-    for (auto &iter : IterplayVector)
-    {
+iterobj(std::string("prashanthn  redd y"),   nullptr);
 
-        cout << iter.getStringObject() << endl;
-    }
 
-    return 0;
+
+
+
+
+
+ iterpointer=  nullptr;
+
+
+return 0;
+
 }
