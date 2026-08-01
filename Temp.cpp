ba372c305ae4f4f58cda7b4c57d4c0d7b04359a3
pashi44 <pashireddi@gmail.com>
2026-08-01 13:42:46 -0400

    the stable brach  to  get started moodify

--- Temp.cpp
+++ Temp.cpp
@@ -6,62 +6,14 @@
 #include <unistd.h>
 using namespace std;
 
-class Abstract {
-    public:
-        virtual void foo() = 0;
-        char c = 'A';
-    };
-    
-    class Derived : public Abstract {
-    public:
-        int b = 42;
-        void foo() override {
-            cout << "Derived::foo" << endl;
-        }
-    };
-    
-    class Thirved : public Abstract {
-    public:
-        int x = 99;
-        void foo() override {
-            cout << "Thirved::foo" << endl;
-        }
-    };
 
 
 
 
+int main(){
 
 
 
-    int main() {
-        Derived d;
-        Thirved t;
-    
-        Abstract* ptr1 = &d;
-        Abstract* ptr2 = &t;
-    
-        // Access vptrs
-        void** vptr_d = *(void***)&d;
-        void** vptr_t = *(void***)&t;
-    
-        cout << "Object d address:         " << &d << endl;
-        cout << "vptr for Derived:         " << vptr_d << endl;
-        cout << "Derived::vtable[0] (RTTI):" << vptr_d[0] << endl;
-        cout << "Derived::vtable[1] (foo): " << vptr_d[1] << endl;
-    
-        cout << "Object t address:         " << &t << endl;
-        cout << "vptr for Thirved:         " << vptr_t << endl;
-        cout << "Thirved::vtable[0] (RTTI):" << vptr_t[0] << endl;
-        cout << "Thirved::vtable[1] (foo): " << vptr_t[1] << endl;
-    
-        // Call virtual via vtable manually
-        using FnType = void(*)(Abstract*);
-        FnType dfunc = (FnType)vptr_d[1];
-        FnType tfunc = (FnType)vptr_t[1];
-    
-        dfunc(ptr1); // calls Derived::foo
-        tfunc(ptr2); // calls Thirved::foo
-    
-        return 0;
+
+    return 0;
     }
